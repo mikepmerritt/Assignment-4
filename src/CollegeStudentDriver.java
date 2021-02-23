2fd9c8798360747a2bc60069ace0cd92efe8b93f
mikepmerritt <mikepmerritt@gmail.com>
2021-02-23 03:24:50 -0500

    replaced Michael with user-input student

--- src/CollegeStudentDriver.java
+++ src/CollegeStudentDriver.java
@@ -1,20 +1,33 @@
+import java.util.Scanner;
 
 public class CollegeStudentDriver {
 
 	public static void main(String[] args) {
 		
-		CollegeStudent michael = new CollegeStudent("Michael", 19, "Quinnipiac");
-		System.out.println(michael);
-		System.out.println("-------------------------------------------");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String name = scan.nextLine();
+		System.out.println("Enter your age:");
+		int age = scan.nextInt();
+		System.out.println("Enter your college:");
+		scan.nextLine();
+		String college = scan.nextLine();
 		
-		for(int i = 1; i <= 4; i++) {
-			System.out.println("Day " + i);
-			michael.goToLecture();
-			michael.goToLecture();
-			michael.eatLunch();
-			michael.goToLecture();
-			michael.doAssignment();
-			System.out.println("-------------------------------------------");
-		}
+		CollegeStudent user = new CollegeStudent(name, age, college);
+		user.goToLecture();
+		
+//		CollegeStudent michael = new CollegeStudent("Michael", 19, "Quinnipiac");
+//		System.out.println(michael);
+//		System.out.println("-------------------------------------------");
+//		
+//		for(int i = 1; i <= 4; i++) {
+//			System.out.println("Day " + i);
+//			michael.goToLecture();
+//			michael.goToLecture();
+//			michael.eatLunch();
+//			michael.goToLecture();
+//			michael.doAssignment();
+//			System.out.println("-------------------------------------------");
+//		}
 	}
 }
